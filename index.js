47bdea66c0b248867d520200853b84307c924c50
Akshay Parmar <121486242+Akshay25092002@users.noreply.github.com>
2025-02-19 10:27:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import cors from "cors";
+//import cors from "cors";
 import dotenv from "dotenv";
 import connectToMongo from "./db.js";
 import userRoutes from "./routes/user.js"
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors());
+//app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
